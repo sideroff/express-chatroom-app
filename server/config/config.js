b2bd8685bc8ae77ba4440deed370386985e22329
sideroff <ivan.sideroff@gmail.com>
2017-01-31 22:42:36 +0200

    fixed: heroku minor changes

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,8 @@ module.exports = {
     development: {
         rootPath: rootPath,
         port: 3000,
-        dbConnectionString: 'mongodb://localhost:27017/chatroom-app'
+        dbConnectionString: 'mongodb://localhost:27017/chatroom-app'        
+        //dbConnectionString: 'mongodb://' + remoteDbUsername + ':' + remoteDbPassword + '@ds035503.mlab.com:35503/chatroom'
     },
     production: {        
         rootPath: rootPath,
